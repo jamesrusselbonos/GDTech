9374e380fbc237ea42af58858ad212ee908ea0c0
michele-joy <adrianbenitez42@gmail.com>
2019-04-22 15:36:45 +0800

    financing

---
+++ resources/views/financing.blade.php
@@ -0,0 +1,25 @@
+@extends('header_footer')
+
+@section('content')
+   
+    <div class="row">
+        <div class="content_11">
+            <div class="container">
+                <div class="col-lg-12">
+
+                    </BR> </BR> </BR>
+                    <h2>OUR TEAM</h2>
+                    <p style="text-align: justify;">We are Global Dynamic Technology (GDTECK), a U.S. distributor of indoor, outdoor and rental LED signs. We are committed to inspiring the globe through continuous innovation. We work diligently to provide the world with leading LED sign technology. We provide efficient, durable, long lasting LED signs that beautifully cater to your specific business needs. Our highly skilled team of software engineers is constantly improving our LED components and software to ensure that our customers have the best operating system available..</p></br>
+
+            <p style="">Get more information about our LED signs and click the button to inquire now.</p>
+
+            	<button class="button" style="vertical-align:middle"><span>CONTACT US</span></button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    
+@endsection
