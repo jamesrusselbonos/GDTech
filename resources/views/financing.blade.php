d5119b7f859d99c43274c5c17c5c4288bed41ce2
michele-joy <adrianbenitez42@gmail.com>
2019-04-22 16:40:42 +0800

    financing

--- resources/views/financing.blade.php
+++ resources/views/financing.blade.php
@@ -1,25 +1,55 @@
 @extends('header_footer')
 
 @section('content')
-   
-    <div class="row">
-        <div class="content_11">
-            <div class="container">
-                <div class="col-lg-12">
+        
+<div class="row">
+        <div class="content_4">
+            <div class="banner">
+                 <div class="container">
+                     <div class="col-lg-12">
+                         <h2>Vital Facts and Information on Getting Easy Loan</h2>
+                         <p>Most people deal with financial struggles at some point in their life. Some circumstances may be worse than others, but regardless of the situation, there are many options to be considered during those times. One of these options is to get an easy loan.</br></br>
 
-                    </BR> </BR> </BR>
-                    <h2>OUR TEAM</h2>
-                    <p style="text-align: justify;">We are Global Dynamic Technology (GDTECK), a U.S. distributor of indoor, outdoor and rental LED signs. We are committed to inspiring the globe through continuous innovation. We work diligently to provide the world with leading LED sign technology. We provide efficient, durable, long lasting LED signs that beautifully cater to your specific business needs. Our highly skilled team of software engineers is constantly improving our LED components and software to ensure that our customers have the best operating system available..</p></br>
-
-            <p style="">Get more information about our LED signs and click the button to inquire now.</p>
-
-            	<button class="button" style="vertical-align:middle"><span>CONTACT US</span></button>
-
-                </div>
+                        Whether you have an emergency that requires immediate financing or you simply want to make a large purchase, getting a loan can be a good option.  If you’re skeptical, here are a few things to consider.
+                        </p>
+                        <button class="button" style="vertical-align:middle"><span>APPLY FOR FINANCING</span></button>
+                     </div>
+                 </div>
             </div>
         </div>
     </div>
 
+    <div class="row">
+         <div class="content_2">
+             <div class="container">
+                 <div class="col-lg-6">
+                     <h3>Why Should you Consider a Loan?</h3>
+                     <p>Every business, no matter what industry, has its own specific needs from working capital to technology and equipment and much more. Getting a loan is a great option to meet your financial needs. Other reasons you need to consider a loan include:.</p>
+                      <p>Reasonable monthly payments. Many people cannot afford to pay cash for large ticket items. This makes financing that item a more significant option than having to pay in bulk all at once. When you get a loan, you can focus on reasonable monthly payments.
+                      Loan to own. Monthly payments are equivalent to most advertising budgets, but eventually you will own it out right and have permanent advertising..</p>
+                 </div>
 
-    
+                 <div class="col-lg-6">
+                     <h3>How to Apply for a Loan?</h3>
+                     <p>Some think that applying for a loan is a very difficult process, but it actually isn’t. Simply fill out our application below and a team member will contact you. We will walk you through every step of the way, ensuring that you obtain the best quality assistance you need..</p><br>
+
+
+
+                     
+                 </div>
+             </div>
+         </div>
+    </div>
+    <div class="container">
+                     <div class="col-lg-12">
+                         <h2>Vital Facts and Information on Getting Easy Loan</h2>
+                         <p>Most people deal with financial struggles at some point in their life. Some circumstances may be worse than others, but regardless of the situation, there are many options to be considered during those times. One of these options is to get an easy loan.</br></br>
+
+                        Whether you have an emergency that requires immediate financing or you simply want to make a large purchase, getting a loan can be a good option.  If you’re skeptical, here are a few things to consider.
+                        </p>
+                      
+                     </div>
+                 </div>
+
+
 @endsection
