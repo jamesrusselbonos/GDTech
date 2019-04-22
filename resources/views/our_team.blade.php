2fe611c65df62e178a98dbc8906c09a13ff01ea8
michele-joy <adrianbenitez42@gmail.com>
2019-04-22 14:32:26 +0800

    our team

---
+++ resources/views/our_team.blade.php
@@ -0,0 +1,25 @@
+@extends('header_footer')
+
+@section('content')
+   
+    <div class="row">
+        <div class="content_11">
+            <div class="container">
+                <div class="col-lg-12">
+
+                    </BR> </BR> </BR>
+                    <h2>OUR TEAM</h2>
+                    <p style="text-align: justify;">“Uphold steadfast faith, defend highest integrity, maintain exceptional quality, pursue endless advancement. Empower our world with efficiency and versatility.”.</p></br>
+
+            <p style="">Get more information about our LED signs and click the button to inquire now.</p>
+
+            	<button class="button" style="vertical-align:middle"><span>CONTACT US</span></button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    
+@endsection
