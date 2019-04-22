035a0f81e416c2f996ef194a308eec78cb7fe560
michele-joy <adrianbenitez42@gmail.com>
2019-04-22 14:14:32 +0800

    button

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -7,7 +7,12 @@
             <div class="container">
                 <div class="col-lg-12">
                     <h2>ABOUT GDTEK</h2>
-                    <p>Global Dynamic Technology (GDTEK) is dedicated to designing, manufacturing, and distributing cutting-edge technology. We are continuously expanding our innovative capabilities and pushing the limits of programmable LED signs. As an innovative leader in the design of the highest LED sign technology available, we use the best components, perform ongoing testing, and implement product enhancements. One of our current focuses in technology is our state-of-the-art LED panel designs and smart cloud-based software. For over 12 years, our manufacturing team has successfully designed, developed, manufactured and sold LED panels on four continents worldwide in over 80 countries and territories. We are a world class manufacturer with strict quality standards. GDTEK acquires leading technology from around the world. We research and develop the latest innovative technology, but we only implement the best cutting-edge technology into our product lines. We commit ourselves to always strive to produce the best, most reliable, energy efficient technology. We are inspiring the globe with GDTEK technology.</p>
+                    <p style="text-align: justify;">Global Dynamic Technology (GDTEK) is dedicated to designing, manufacturing, and distributing cutting-edge technology. We are continuously expanding our innovative capabilities and pushing the limits of programmable LED signs. As an innovative leader in the design of the highest LED sign technology available, we use the best components, perform ongoing testing, and implement product enhancements. One of our current focuses in technology is our state-of-the-art LED panel designs and smart cloud-based software. For over 12 years, our manufacturing team has successfully designed, developed, manufactured and sold LED panels on four continents worldwide in over 80 countries and territories. We are a world class manufacturer with strict quality standards. GDTEK acquires leading technology from around the world. We research and develop the latest innovative technology, but we only implement the best cutting-edge technology into our product lines. We commit ourselves to always strive to produce the best, most reliable, energy efficient technology. We are inspiring the globe with GDTEK technology.</p></br>
+
+            <p style="">Get more information about our LED signs and click the button to inquire now.</p>
+
+            	<button class="button" style="vertical-align:middle"><span>CONTACT US</span></button>
+
                 </div>
             </div>
         </div>
