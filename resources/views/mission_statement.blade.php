cc8af8c6d9a165ee72c58f62a69b699ae4d09f73
michele-joy <adrianbenitez42@gmail.com>
2019-04-22 16:45:18 +0800

    fourth major update

--- resources/views/mission_statement.blade.php
+++ resources/views/mission_statement.blade.php
@@ -6,8 +6,10 @@
         <div class="content_11">
             <div class="container">
                 <div class="col-lg-12">
-                    <h2>ABOUT GDTEK</h2>
-                    <p style="text-align: justify;">Global Dynamic Technology (GDTEK) is dedicated to designing, manufacturing, and distributing cutting-edge technology. We are continuously expanding our innovative capabilities and pushing the limits of programmable LED signs. As an innovative leader in the design of the highest LED sign technology available, we use the best components, perform ongoing testing, and implement product enhancements. One of our current focuses in technology is our state-of-the-art LED panel designs and smart cloud-based software. For over 12 years, our manufacturing team has successfully designed, developed, manufactured and sold LED panels on four continents worldwide in over 80 countries and territories. We are a world class manufacturer with strict quality standards. GDTEK acquires leading technology from around the world. We research and develop the latest innovative technology, but we only implement the best cutting-edge technology into our product lines. We commit ourselves to always strive to produce the best, most reliable, energy efficient technology. We are inspiring the globe with GDTEK technology.</p></br>
+
+                </BR> </BR> </BR>
+                    <h2>MISSION STATEMENT</h2>
+                    <p style="text-align: justify;">“Uphold steadfast faith, defend highest integrity, maintain exceptional quality, pursue endless advancement. Empower our world with efficiency and versatility.”.</p></br>
 
             <p style="">Get more information about our LED signs and click the button to inquire now.</p>
 
--- resources/views/our_team.blade.php
+++ resources/views/our_team.blade.php
@@ -9,7 +9,7 @@
 
                     </BR> </BR> </BR>
                     <h2>OUR TEAM</h2>
-                    <p style="text-align: justify;">“Uphold steadfast faith, defend highest integrity, maintain exceptional quality, pursue endless advancement. Empower our world with efficiency and versatility.”.</p></br>
+                    <p style="text-align: justify;">We are Global Dynamic Technology (GDTECK), a U.S. distributor of indoor, outdoor and rental LED signs. We are committed to inspiring the globe through continuous innovation. We work diligently to provide the world with leading LED sign technology. We provide efficient, durable, long lasting LED signs that beautifully cater to your specific business needs. Our highly skilled team of software engineers is constantly improving our LED components and software to ensure that our customers have the best operating system available..</p></br>
 
             <p style="">Get more information about our LED signs and click the button to inquire now.</p>
 
