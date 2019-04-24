e6bc795323894720026212efcfb42198707a78cc
michele-joy <adrianbenitez42@gmail.com>
2019-04-24 17:36:55 +0800

    link

---
+++ resources/views/water_proofing.blade.php
@@ -0,0 +1,25 @@
+@extends('header_footer')
+
+@section('content')
+
+		<div class="row">
+         <div class="content_2">
+             <div class="container">
+                 <div class="col-lg-62" style="padding-top: 50px;padding-bottom: 50px;">
+                     <h3 style="text-align: center;" >Selecting the Right Sign</h3>
+                     <p style="text-align: justify;">Believe it or not, finding the right digital LED sign for your business takes a little more thought than most people imagine. Choosing the right sized LED model with proper resolution for your business depends on a few key questions. For example, where will your digital sign will be installed? Will your sign be seen by pedestrians walking close by at a slow speed or by vehicles far away moving at fast speeds? Will you display pictures, videos, or both? Our professional team at GDTEK can help you assess the answers to these questions and choose the best LED sign for your business.</p>
+                 </div>
+
+                 <div class="col-lg-6" style="text-align: justify;">
+                     <h3>Pitch, Resolution, Viewing Distance, and Angle</h3>
+                     <p>The resolution of your LED sign ultimately depends on the particular pixel pitch of the model you choose. A smaller pixel pitch means that there is a larger number of pixels on a smaller surface area. This translates to a higher resolution which is ideal for viewing at a close proximity near eye level. A larger pixel pitch means that there is a smaller number of pixels on a larger surface area. This translates to a lower resolution which is ideal for seeing from a far distance high above ground level.</p>
+                 </div>
+                 <div class="col-lg-6">
+                     <img src="/img/Sales-Materials.png" alt="Snow" style="width:100%;max-height: 500px;">
+                 </div>
+             </div>
+         </div>
+    </div>
+
+
+@endsection
