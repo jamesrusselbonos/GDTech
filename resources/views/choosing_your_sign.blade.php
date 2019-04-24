03e7fe6e173b3432f02f141837d0f2daee681a37
michele-joy <adrianbenitez42@gmail.com>
2019-04-24 10:35:57 +0800

    change

--- resources/views/choosing_your_sign.blade.php
+++ resources/views/choosing_your_sign.blade.php
@@ -1,25 +1,75 @@
 @extends('header_footer')
 
 @section('content')
-   
-<div class="content_cover"></div>
 
-    <div class="row">
-        <div class="content_11">
-            <div class="container">
-                <div class="col-lg-12">
-                    <h2>ABOUT GDTEK</h2>
-                    <p style="text-align: justify;">Global Dynamic Technology (GDTEK) is dedicated to designing, manufacturing, and distributing cutting-edge technology. We are continuously expanding our innovative capabilities and pushing the limits of programmable LED signs. As an innovative leader in the design of the highest LED sign technology available, we use the best components, perform ongoing testing, and implement product enhancements. One of our current focuses in technology is our state-of-the-art LED panel designs and smart cloud-based software. For over 12 years, our manufacturing team has successfully designed, developed, manufactured and sold LED panels on four continents worldwide in over 80 countries and territories. We are a world class manufacturer with strict quality standards. GDTEK acquires leading technology from around the world. We research and develop the latest innovative technology, but we only implement the best cutting-edge technology into our product lines. We commit ourselves to always strive to produce the best, most reliable, energy efficient technology. We are inspiring the globe with GDTEK technology.</p></br>
 
-            <p style="">Get more information about our LED signs and click the button to inquire now.</p>
 
-            	<button class="button" style="vertical-align:middle"><span>CONTACT US</span></button>
+<div class="hero-image">
+     <img src="/img/banner.jpg" alt="Snow" style="width:100%;max-height: 500px;">
+ <div class="hero-text">
+    <h1 style="font-size:40px">Choosing Your Sign</h1>
+    <a href="#" style="color: #fff">Home</a> >> <a href="#" style="color: #fff">LED Basics</a> >> <a href="/choosing_your_sign" style="color: #fff">Choosing Your Sign</a>
+  </div>
+</div>
+    
+<div class="row">
+         <div class="content_2">
+             <div class="container">
+                 <div class="col-lg-62" style="padding-top: 50px;padding-bottom: 50px;">
+                     <h3 style="text-align: center;" >Selecting the Right Sign</h3>
+                     <p style="text-align: justify;">Believe it or not, finding the right digital LED sign for your business takes a little more thought than most people imagine. Choosing the right sized LED model with proper resolution for your business depends on a few key questions. For example, where will your digital sign will be installed? Will your sign be seen by pedestrians walking close by at a slow speed or by vehicles far away moving at fast speeds? Will you display pictures, videos, or both? Our professional team at GDTEK can help you assess the answers to these questions and choose the best LED sign for your business..</p>
+                 </div>
 
-                </div>
-            </div>
-        </div>
+                 <div class="col-lg-6" style="text-align: justify;">
+                     <h3>Pitch, Resolution, Viewing Distance, and Angle</h3>
+                     <p>The resolution of your LED sign ultimately depends on the particular pixel pitch of the model you choose. A smaller pixel pitch means that there is a larger number of pixels on a smaller surface area. This translates to a higher resolution which is ideal for viewing at a close proximity near eye level. A larger pixel pitch means that there is a smaller number of pixels on a larger surface area. This translates to a lower resolution which is ideal for seeing from a far distance high above ground level.</p>
+                 </div>
+                 <div class="col-lg-6">
+                     <img src="/img/Sales-Materials.png" alt="Snow" style="width:100%;max-height: 500px;">
+                 </div>
+             </div>
+         </div>
     </div>
+</div>
+    <div class="row">
+         <div class="content_2">
+             <div class="container">
+                
+                        
+                 <div class="col-lg-6" style="text-align: justify;">
+                     <h3>What Size Sign?</h3>
+                     <p>To determine the appropriate size for your location, take into consideration the speed and type of traffic, distance from the roadway, space restrictions, zoning constraints and desired sign content.</p>
+                 </div>
+                     <div class="col-lg-6">
+                     <img src="/img/Sales-Materials.png" alt="Snow" style="width:100%;max-height: 500px;">
+                 </div>
+             </div>
+         </div>
+    </div>
+</div>
+    <div class="row">
+         <div class="content_2">
+             <div class="container">
+                
 
+                 <div class="col-lg-6" style="text-align: justify;">
+                     <h3>Budget</h3>
+                     <p>As with most big purchases, quality, features and size will all affect the price of your LED sign. It’s important to remember that an LED sign should not be viewed as a capital investment. Instead, look to your marketing and advertising budgets. Unlike traditional advertising methods, LED signs offer incredible value and ongoing return on investment. Consider this: when you invest in an LED sign, you control the “publication” of your message. You won’t lose time and money getting an ad into circulation. Instead, your advertising will reach people when they’re most able to act on it. We’ve seen business owners sacrifice quality for price and purchase an inferior, imported product only to have it replaced a few months later. A low price can be enticing, but your initial savings may cost a lot more than you bargained for. The fact is your investment in an LED sign is just too large to gamble your success on a cut-rate product.</p>
+                 </div>
+                 <div class="col-lg-6">
+                     <img src="/img/Sales-Materials.png" alt="Snow" style="width:100%;max-height: 500px;">
+                 </div>
+             </div>
+         </div>
+    </div>
+        <div class="container">
+                 <div class="col-lg-62" style="padding-top: 50px;padding-bottom: 50px;">
+                     <h3 style="text-align: center;" >Selecting the Right Sign</h3>
+                     <p style="text-align: justify;">Believe it or not, finding the right digital LED sign for your business takes a little more thought than most people imagine. Choosing the right sized LED model with proper resolution for your business depends on a few key questions. For example, where will your digital sign will be installed? Will your sign be seen by pedestrians walking close by at a slow speed or by vehicles far away moving at fast speeds? Will you display pictures, videos, or both? Our professional team at GDTEK can help you assess the answers to these questions and choose the best LED sign for your business..</p>
+                 </div>
+
+               
+             </div>
 
     
 @endsection
