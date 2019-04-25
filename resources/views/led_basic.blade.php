cff7d1b64431531f8b76ca8f12062f7ab950c785
michele-joy <adrianbenitez42@gmail.com>
2019-04-25 10:17:13 +0800

    link link

---
+++ resources/views/led_basic.blade.php
@@ -0,0 +1,55 @@
+@extends('header_footer')
+
+@section('content')
+
+
+
+<div class="hero-image">
+     <img src="/img/jael-vallee-1455237-unsplash.jpg" alt="Snow" style="width:100%;max-height: 600px;">
+ <div class="hero-text">
+    <h1 style="font-size:35px">Water Proofing</h1>
+    <a href="#" style="color: #fff">Home</a> >> <a href="#" style="color: #fff">LED Basics</a> >> <a href="/choosing_your_sign" style="color: #fff ">Water Proofing</a>
+  </div>
+</div>
+    
+    <div class="row">
+         <div class="content_2">
+             <div class="container">
+                 <div class="col-lg-62" style="padding-top: 50px;padding-bottom: 50px;">
+                     <h3 style="text-align: center;" >Water Proofing</h3>
+                     <p style="text-align: justify;">Our Led panels are all kept at a high standard of IP66 waterproofing. This allows us to have confidence that our panels can easily withstand all weather conditions rain or shine.</p>
+                 </div>
+
+                 <div class="col-lg-6" style="text-align: justify;">
+                     <h3>Fish Tank Demonstration</h3>
+                     <p>This is our panel completely submerged in a fish tank.</p>
+                    <iframe width="470" height="315" src="https://www.youtube.com/embed/_cz2pP0fr1E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></br></br>
+                 </div>
+                 <div class="col-lg-6">
+                     <h3>GDtech Easy Installation</h3>
+                     <p>This is our panel completely submerged in a fish tank.</p>
+                    <iframe width="470" height="315" src="https://www.youtube.com/embed/bGn-j51sXi8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></br></br>
+                 </div>
+             </div>
+         </div>
+    </div>
+
+    
+   
+        <div class="container">
+                 <div class="col-lg-62" style="padding-top: 50px;padding-bottom: 50px;">
+                     <h3 style="text-align: center;" >LED Panel Water Proof Jet Spray Test</h3>
+                     <center><iframe width="970" height="315" src="https://www.youtube.com/embed/Yv25x5EOpn4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+                 </div>
+             </div>
+             <div class="container">
+                 <div class="col-lg-62" style="padding-top: 50px;padding-bottom: 50px;">
+                     <h3 style="text-align: center;" >Silicon Injection Water Proofing</h3>
+                     <center><p>This is a demonstration of our machine injecting silicon to help improve the water proofing of our panels.</p></center>
+                    <center><iframe width="970" height="315" src="https://www.youtube.com/embed/4IHB_WC9CGw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+                 </div>
+             </div>
+      
+
+    
+@endsection
