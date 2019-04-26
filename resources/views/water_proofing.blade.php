ccaa2d04cb0ec54433fa69605c8e3cfeb5697f77
michele-joy <adrianbenitez42@gmail.com>
2019-04-26 09:59:32 +0800

    water_proofing page

--- resources/views/water_proofing.blade.php
+++ resources/views/water_proofing.blade.php
@@ -2,24 +2,54 @@
 
 @section('content')
 
-		<div class="row">
+
+
+<div class="hero-image">
+     <img src="/img/jael-vallee-1455237-unsplash.jpg" alt="Snow" style="width:100%;max-height: 600px;">
+ <div class="hero-text">
+    <h1 style="font-size:35px">Water Proofing</h1>
+    <a href="#" style="color: #fff">Home</a> >> <a href="#" style="color: #fff">LED Basics</a> >> <a href="/choosing_your_sign" style="color: #fff ">Water Proofing</a>
+  </div>
+</div>
+    
+    <div class="row">
          <div class="content_2">
              <div class="container">
                  <div class="col-lg-62" style="padding-top: 50px;padding-bottom: 50px;">
-                     <h3 style="text-align: center;" >Selecting the Right Sign</h3>
-                     <p style="text-align: justify;">Believe it or not, finding the right digital LED sign for your business takes a little more thought than most people imagine. Choosing the right sized LED model with proper resolution for your business depends on a few key questions. For example, where will your digital sign will be installed? Will your sign be seen by pedestrians walking close by at a slow speed or by vehicles far away moving at fast speeds? Will you display pictures, videos, or both? Our professional team at GDTEK can help you assess the answers to these questions and choose the best LED sign for your business.</p>
+                     <h3 style="text-align: center;" >Water Proofing</h3>
+                     <p style="text-align: justify;">Our Led panels are all kept at a high standard of IP66 waterproofing. This allows us to have confidence that our panels can easily withstand all weather conditions rain or shine.</p>
                  </div>
 
                  <div class="col-lg-6" style="text-align: justify;">
-                     <h3>Pitch, Resolution, Viewing Distance, and Angle</h3>
-                     <p>The resolution of your LED sign ultimately depends on the particular pixel pitch of the model you choose. A smaller pixel pitch means that there is a larger number of pixels on a smaller surface area. This translates to a higher resolution which is ideal for viewing at a close proximity near eye level. A larger pixel pitch means that there is a smaller number of pixels on a larger surface area. This translates to a lower resolution which is ideal for seeing from a far distance high above ground level.</p>
+                     <h3>Fish Tank Demonstration</h3>
+                     <p>This is our panel completely submerged in a fish tank.</p>
+                    <iframe width="470" height="315" src="https://www.youtube.com/embed/_cz2pP0fr1E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></br></br>
                  </div>
                  <div class="col-lg-6">
-                     <img src="/img/Sales-Materials.png" alt="Snow" style="width:100%;max-height: 500px;">
+                     <h3>GDtech Easy Installation</h3>
+                     <p>This is our panel completely submerged in a fish tank.</p>
+                    <iframe width="470" height="315" src="https://www.youtube.com/embed/bGn-j51sXi8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></br></br>
                  </div>
              </div>
          </div>
     </div>
 
+    
+   
+        <div class="container">
+                 <div class="col-lg-62" style="padding-top: 50px;padding-bottom: 50px;">
+                     <h3 style="text-align: center;" >LED Panel Water Proof Jet Spray Test</h3>
+                     <center><iframe width="970" height="315" src="https://www.youtube.com/embed/Yv25x5EOpn4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+                 </div>
+             </div>
+             <div class="container">
+                 <div class="col-lg-62" style="padding-top: 50px;padding-bottom: 50px;">
+                     <h3 style="text-align: center;" >Silicon Injection Water Proofing</h3>
+                     <center><p>This is a demonstration of our machine injecting silicon to help improve the water proofing of our panels.</p></center>
+                    <center><iframe width="970" height="315" src="https://www.youtube.com/embed/4IHB_WC9CGw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+                 </div>
+             </div>
+      
 
+    
 @endsection
