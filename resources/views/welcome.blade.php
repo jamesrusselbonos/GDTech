f680e6fc9589f7da241eb6c25a2f7131bd8162b1
michele-joy <adrianbenitez42@gmail.com>
2019-05-08 08:23:58 +0800

    css_welcome_img

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -260,7 +260,49 @@
                <div class="row">
                 <div class="col-lg-12">
                   <div class="cat_header">
-                     <h1>The Processor</h1>
+                     <h1>Module</h1>
+                    
+                  </div>
+                </div>
+               </div>
+               <div class="row">
+                 <div style="text-align: left; padding-top: 80px;" class="col-lg-3">
+                  <ul>
+                      <li>Stablized Signal Connector</li>
+                      <li>Super Heat Dissipation</li>
+                      <li>Layout & Design Produced High Contrast</li>
+                      <li>Rated For All Climates</li>
+                     
+                   </ul>
+                 </div>
+                 <div class="col-lg-5">
+                   <div class="processor_img2"></div>
+                 </div>
+                 <div style="padding-top: 200px;" class="col-lg-4">
+                   <ul>
+                      <li>Materials Fussion Seals Circuit</li>
+                      <li>Aluminum Frame protects PCB Layout</li>
+                      <li>Thermo Paste<strong> Ensure Operation Boiling Water</strong></li>
+                      <li>Protection Achieves Waterproof Highest Rating of IP68</strong></li>
+                      <li><strong>Fire Rating Protection</strong></li>
+                   </ul>
+                 </div>
+               </div>
+               </div>
+             </div>
+           </div>
+        </div>
+            
+            <hr>
+            
+            <div class="row">
+           <div class="catalog_processor">
+             <div class="row">
+               <div class="container">
+               <div class="row">
+                <div class="col-lg-12">
+                  <div class="cat_header">
+                     <h1> Processor</h1>
                     <h4>ADVANCED &bull; STABLE &bull; SMART &bull; SIMPLE</h4>
                   </div>
                 </div>
@@ -270,7 +312,7 @@
                    <h3>Higher Color Spectrum & Resolution</h3>
                  </div>
                  <div class="col-lg-5">
-                   <div class="processor_img"></div>
+                   <div class="processor_img1"></div>
                  </div>
                  <div style="padding-top: 200px;" class="col-lg-4">
                    <ul>
@@ -286,6 +328,55 @@
              </div>
            </div>
         </div>
+
+              <hr>
+
+                <div class="row">
+           <div class="catalog_processor">
+             <div class="row">
+               <div class="container">
+               <div class="row">
+                <div class="col-lg-12">
+                  <div class="cat_header">
+                     <h1>Cabinet</h1>
+                  <h1 style="text-align: center;font-size: 13px;">Standard Led Signs whither under Heat and Light of the sun.</h1>
+                  <h1 style="text-align: center;font-size: 13px;margin-top: -10px;"><strong>Gdtect's Cabinet Remains Unchanged after 15 years without the use as a constantly failing fan system.</strong></h1>
+                    
+                  </div>
+                </div>
+               </div>
+               <div class="row">
+                 <div style="text-align: left; padding-top: 80px;" class="col-lg-3">
+                  
+                 </div>
+                 <div class="col-lg-5">   
+                   <div class="processor_img2"></div>
+                 </div>
+                 <div style="padding-top: 200px;" class="col-lg-4">
+             
+                          <div class="row">
+                            
+                            <div class="col-md-6"> <p><i class="fas fa-bolt" style="font-size: 50px;"></i> LOW ENERGY</p></div>
+                             <div class="col-md-6"> <p><i class="fas fa-thermometer-empty" style="font-size: 50px;"></i> LOW HEAT</p></div>
+                     
+                          </div>
+                               <hr>
+                   <ul>
+                      <li><strong>Minimal Energy Use:</strong> The power Consumption is 25% (1/4) of Competion.</li>
+                      <li><strong>No air cinditioning Rquired.</li></strong>
+                      <li><strong>Superior Cast Aluminum Case:</strong> Fully Dissipate Heat, In addition To Lamp and Module.</strong></li>
+                      <li><strong>Completely Encasing</strong> For Protection Against Weather:Water (IP68
+                      -Highest Waterproof Rating), Ultraviolet Light, High winds, High Temperature, and Salty Marine Environments.</strong></li>
+                      
+                   </ul>
+                 </div>
+               </div>
+               </div>
+             </div>
+           </div>
+        </div>
+            
+            <hr>
 
 
    <!--  <div class="contact_form">
